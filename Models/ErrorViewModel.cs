a37af16dc0932d751be221542b3d77450d93965e
jeremy.rawls <jeremy.rawls@AR-HYYSYY2>
2022-01-28 13:12:12 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UTMHR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
